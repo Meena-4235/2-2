f86a9a19fe2ea080d8dcfa135f2edea8b81a7a8e
Meena-4235 <113126864+Meena-4235@users.noreply.github.com>
2024-05-16 11:21:54 +0530

    Counter app

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import React from  'react';
-import ColorChanger from  './colorchanger';
+import React , { useState } from "react";
+import Button from "./Button";
+import "./App.css";
 
-function App(){
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () =>{
+    setCount(count + 1);
+  };
+  let decrementCount = () =>{
+    setCount(count - 1);
+  };
   return (
-    <div>
-      <ColorChanger />
+    <div className="app">
+      <div>
+      <div className="title-container">
+        <h2>COUNTER REACT APP</h2>
+        <div className= " title-underline"> </div>
+      </div>
+      <div class="count">
+        <h3>Count:</h3>
+        <h1>{count}</h1>
+      </div>
+      <div class="buttons">
+        <Button title= {"-"} action={decrementCount} />
+        <Button title= {"+"} action={incrementCount} />
+      </div>
     </div>
-  );
-}
-
-export default App;
+    </div>
+    );
+  }
